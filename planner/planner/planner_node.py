4a45b32995adf86229dfa7c235de2ea612f86490
Damian Owerko <Damowerko@users.noreply.github.com>
2023-04-23 22:11:52 +0000

    mpc with planning

--- planner/planner/planner_node.py
+++ planner/planner/planner_node.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 import time
 
-import graph_ltpl
 import numpy as np
 import rclpy
+from graph_ltpl.Graph_LTPL import Graph_LTPL
+from graph_ltpl.imp_global_traj.src.import_globtraj_csv import import_globtraj_csv
 from nav_msgs.msg import Odometry
 from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray
@@ -13,7 +14,7 @@ from std_msgs.msg import Float32MultiArray
 #     raise ValueError('Specified online parameter config file does not exist or is empty!')
 
 
-toppath = "f1ten_ws/src/f1ten-tigers/planner"
+toppath = "/home/damow/ese615/f1ten_ws/src/f1ten-tigers/planner"
 track_specifier = "skir"
 
 # define all relevant paths
@@ -44,7 +45,7 @@ class Planner(Node):
 
         self.sim = True
         self.timer = self.create_timer(0.05, self.timer_callback)
-        self.publisher_array = self.create_publisher(Float32MultiArray, "/localpath", 1)
+        self.publisher_array = self.create_publisher(Float32MultiArray, "/path", 1)
         odom_topic = "/ego_racecar/odom" if self.sim else "/pf/pose/odom"
         self.sub_odom = self.create_subscription(
             Odometry, odom_topic, self.odom_callback, 1
@@ -55,51 +56,59 @@ class Planner(Node):
         # ----------------------------------------------------------------------------------------------------------------------
 
         # intialize graph_ltpl-class
-        self.ltpl_obj = graph_ltpl.Graph_LTPL.Graph_LTPL(
-            path_dict=path_dict, log_to_file=False
+        self.ltpl_obj = Graph_LTPL(
+            path_dict=path_dict, visual_mode=True, log_to_file=False
         )
 
         # calculate offline graph
         self.ltpl_obj.graph_init()
 
         # set start pose based on first point in provided reference-line
-        refline = (
-            graph_ltpl.imp_global_traj.src.import_globtraj_csv.import_globtraj_csv(
-                import_path=path_dict["globtraj_input_path"]
-            )[0]
-        )
+        refline = import_globtraj_csv(import_path=path_dict["globtraj_input_path"])[0]
         self.pos_est = refline[0, :]
-        heading_est = (
+        heading_est = float(
             np.arctan2(np.diff(refline[0:2, 1]), np.diff(refline[0:2, 0])) - np.pi / 2
         )
         self.vel_est = 0.0
+        self.selected_action = "straight"
 
         # set start pos
         self.ltpl_obj.set_startpos(pos_est=self.pos_est, heading_est=heading_est)
         self.traj_set = {"straight": None}
-        self.tic = time.time()
-
-        self.vel_est = 0
-        self.pos_est = [0, 0]
 
     def odom_callback(self, odom_msg):
         self.vel_est = odom_msg.twist.twist.linear.x
         position = odom_msg.pose.pose.position
-        self.pos_est = [position.x, position.y]
+        self.pos_est = np.array([position.x, position.y])
 
-    def timer_callback(self):
+    def choose_action(self, traj_set=None):
         for sel_action in [
-            "right",
             "left",
             "straight",
+            "right",
             "follow",
-        ]:  # try to force 'right', else try next in list
+        ]:
             if sel_action in self.traj_set.keys():
-                break
+                return sel_action
+        return None
 
-        self.ltpl_obj.calc_paths(prev_action_id=sel_action, object_list=[])
+    def get_objects(self):
+        obj1 = {
+            "id": 0,  # integer id of the object
+            "type": "physical",  # type 'physical' (only class implemented so far)
+            "X": 2.5,  # x coordinate
+            "Y": 1.0,  # y coordinate
+            "theta": 0,  # orientation (north = 0.0)
+            "v": 0.0,  # velocity along theta
+            "length": 0.5,  # length of the object
+            "width": 0.5,  # width of the object
+        }
+        return [obj1]
 
-        self.tic = time.time()
+    def timer_callback(self):
+        self.ltpl_obj.calc_paths(
+            prev_action_id=self.selected_action, object_list=self.get_objects()
+        )
 
         # -- CALCULATE VELOCITY PROFILE AND RETRIEVE TRAJECTORIES ----------------------------------------------------------
         # pos_est:[x, y]
@@ -107,28 +116,16 @@ class Planner(Node):
         self.traj_set = self.ltpl_obj.calc_vel_profile(
             pos_est=self.pos_est, vel_est=self.vel_est
         )[0]
-        # print(len(self.traj_set["straight"][0]))
-        print(
-            "x={:.2f},y={:.2f},v={:.2f}".format(
-                self.pos_est[0], self.pos_est[1], self.vel_est
-            )
-        )
+
+        self.selected_action = self.choose_action()
 
         # [s, x, y, heading, curvature, vx, ax]
-        traj_set = self.traj_set["straight"][0]
-        print("length of points=", len(traj_set))
-        traj_set[:, 3] = traj_set[:, 3] + np.pi / 2
-        traj_set = np.array(traj_set)[:, [1, 2, 5, 3]]
-        while len(traj_set) != 9:
-            traj_set = np.vstack((traj_set, traj_set[-1]))
+        trajectory = self.traj_set[self.selected_action][0]
 
-        # print("shape of traj set=",traj_set.shape)
-        print("({:.2f},{:.2f})".format(traj_set[0][0], traj_set[0][1]))
         array_msg = Float32MultiArray()
-        array_msg.data = list(traj_set.flatten())
-        # array_msg.data[2]= list(traj_set[2])
+        array_msg.data = list(trajectory.flatten())
         self.publisher_array.publish(array_msg)
-        # print(traj_set)
+
         # -- LIVE PLOT (if activated) --------------------------------------------------------------------------------------
         self.ltpl_obj.visual()
 
